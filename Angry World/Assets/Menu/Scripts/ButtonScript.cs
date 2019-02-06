7861ba76572523c19a35fb8b5dc79fcbb67308c6
mz <mzuka@Mrikas-MacBook-Pro.local>
2019-02-06 21:05:41 +0100

    mz

---
+++ Angry World/Assets/Menu/Scripts/ButtonScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour {
+
+    public GameObject PlayButton;
+    public GameObject QuitButton;
+    public GameObject menuPanel;
+    public GameObject MessagePanelPlastic;
+    public GameObject MessagePanelAtomCanister;
+    public GameObject MessagePanelTrump;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void playGame()
+    {
+        menuPanel.SetActive(false);
+        MessagePanelPlastic.SetActive(false);
+        MessagePanelAtomCanister.SetActive(false);
+        MessagePanelTrump.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Angry World/Assets/Menu/Scripts/SceneManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour {
+
+    public GameObject menuPanel;
+    public GameObject MessagePanelPlastic;
+    public GameObject MessagePanelAtomCanister;
+    public GameObject MessagePanelTrump;
+
+
+
+    public void Start()
+    {
+        showMenuPanel();
+    }
+
+
+
+
+
+    public void showMenuPanel()
+    {
+        menuPanel.SetActive(true);
+        MessagePanelPlastic.SetActive(false);
+        MessagePanelAtomCanister.SetActive(false);
+        MessagePanelTrump.SetActive(false);
+
+    }
+
+    public void showMessagePanelPlastic()
+    {
+        menuPanel.SetActive(false);
+        MessagePanelPlastic.SetActive(true);
+        MessagePanelAtomCanister.SetActive(false);
+        MessagePanelTrump.SetActive(false);
+    }
+
+    public void showMessagePanelAtomCanister()
+    {
+        menuPanel.SetActive(false);
+        MessagePanelPlastic.SetActive(false);
+        MessagePanelAtomCanister.SetActive(true);
+        MessagePanelTrump.SetActive(false);
+    }
+
+    public void showMessagePanelTrump()
+    {
+        menuPanel.SetActive(false);
+        MessagePanelPlastic.SetActive(false);
+        MessagePanelAtomCanister.SetActive(false);
+        MessagePanelTrump.SetActive(true);
+    }
+}
